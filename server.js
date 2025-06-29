47011aec17d6bb7ffc936e962e2ad5f138148f5e
Michael <michael.fernandez@tecsup.edu.pe>
2025-06-29 14:17:34 -0500

    desplegar

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+      origin: process.env.FRONTEND_URL?.split(',') || ['http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
